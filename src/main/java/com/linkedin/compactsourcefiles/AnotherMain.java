e488daee4ac4d50b4289ac5df331979e9d83528e
maaikevanputten <maaike.vanputten@brightboost.nl>
2025-10-19 21:35:50 +0200

    more mains

---
+++ src/main/java/com/linkedin/compactsourcefiles/AnotherMain.java
@@ -0,0 +1,11 @@
+package com.linkedin.compactsourcefiles;
+
+public class AnotherMain {
+    public static void main(String[] args) {
+        System.out.println("Another hello world");
+    }
+
+    void main() {
+        IO.println("Hello world");
+    }
+}
